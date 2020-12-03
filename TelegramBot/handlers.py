fa1996d04665b20c3b210726780e034cc2565305
Fanix0713 <63342771+Fanix0713@users.noreply.github.com>
2020-12-03 13:19:04 +0300

    Add files via upload

---
+++ TelegramBot/handlers.py
@@ -0,0 +1,16 @@
+# Import library
+from main import bot, dp
+from aiogram import types
+from aiogram.types import Message
+from config import admin_id
+
+# Send message to admin
+async def send_to_admin(dp):
+	await bot.send_message(chat_id=admin_id, text="Bot start")
+
+# Start bot using func
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+        text = "3"
+        await message.answer(text=text)
+
